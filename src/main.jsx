3b56ceb764505ac5f3daf0f5d932c0da0d8840e7
Yushenko Dima <damios.1812@gmail.com>
2025-06-18 10:46:22 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/app/App.jsx'
+import MarvelService from './services/MarvelService.jsx'
+
+import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+// marvelService.getAllCharacters().then(res => res.data.results.forEach(element => {
+//   console.log(element.name);
+// }));
+
+marvelService.getCharacter(1).then(res => console.log(res));
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
