d18539704aebecd03d97080c6d96064069b46dbf
Yushenko Dima <damios.1812@gmail.com>
2025-06-19 11:10:30 +0200

    added function for API randomChar

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/app/App.jsx'
-import MarvelService from './services/MarvelService.jsx'
+// import MarvelService from './services/MarvelService.jsx'
 
 import './style/style.scss';
 
-const marvelService = new MarvelService();
+// marvelService = new MarvelService();
 
 // marvelService.getAllCharacters().then(res => res.data.results.forEach(element => {
 //   console.log(element.name);
 // }));
 
-marvelService.getCharacter(1).then(res => console.log(res));
+// marvelService.getCharacter(1).then(res => console.log(res));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
